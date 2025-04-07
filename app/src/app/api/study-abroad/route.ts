c1e78242360fd8586c0c444577ca19ed1c19ce3d
rmolina4 <rrmolina4@gmail.com>
2025-04-07 08:26:19 -0400

    changed file structure

---
+++ app/src/app/api/study-abroad/route.ts
@@ -0,0 +1,8 @@
+import connectMongoDB from "@/libs/mongodb";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import mongoose from "mongoose";
+
+export async function GET(request: NextRequest) {
+    // Handle GET requests
+}
