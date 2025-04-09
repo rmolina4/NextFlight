0030f09cd65e1f8b90b6f25db7d3b3acabbe95f7
Steven Tran <stevenhuyt@gmail.com>
2025-04-09 15:38:08 -0400

    Created a component for studyabroad

--- app/src/app/(main)/page.tsx
+++ app/src/app/(main)/page.tsx
@@ -1,16 +1,9 @@
 import Image from 'next/image';
 import plane from '@/../public/images/plane.jpg';
+import Studyabroad from "../../components/studyabroad";
 
 export default function Home() {
-  return (
-    <div className="relative w-full h-screen">
-      <Image
-        src={plane}
-        alt="Plane"
-        layout="fill"
-        objectFit="cover"
-      />
-      <div className="absolute bottom-0 left-0 w-full h-2/3 bg-gradient-to-t from-white/90 to-transparent" />
-    </div>
-  );
+  <div>
+    <Studyabroad/>
+  </div>
 }
---
+++ app/src/components/studyabroad.tsx
@@ -0,0 +1,7 @@
+export default function Studyabroad() {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
