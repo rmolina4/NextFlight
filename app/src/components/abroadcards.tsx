c388f01b301726597fb9c7bcfc04f57ede365122
Steven Tran <stevenhuyt@gmail.com>
2025-04-09 17:21:10 -0400

    Makes study abroad program component

---
+++ app/src/components/abroadcards.tsx
@@ -0,0 +1,10 @@
+
+export default function Abroadcards() {
+    return (
+        <div className="IndivCard">
+            <h1>Program #</h1>
+            <p> Location</p>
+            <a href="Google.com"> Image</a>
+        </div>
+    )
+}
--- app/src/components/studyabroad.tsx
+++ app/src/components/studyabroad.tsx
@@ -1,7 +1,17 @@
+import Abroadcards from "./abroadcards";
+
 export default function Studyabroad() {
     return (
         <div>
-            <h1>hello</h1>
+            <h1 className="AbroadTitle"> Study Abroad Programs</h1>
+            <div className="abroadContainer">
+            <Abroadcards/>
+            <Abroadcards/>
+            <Abroadcards/>
+            <Abroadcards/>
+            <Abroadcards/>
+            <Abroadcards/>
+            </div>
         </div>
     )
 }
