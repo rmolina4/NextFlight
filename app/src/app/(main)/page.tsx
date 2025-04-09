541498991ca7e5ec64afb89fedfd922a12b2b686
Amir Cox <amircoxx31@gmail.com>
2025-04-09 17:13:45 -0400

    updated searchbar

--- app/src/app/(main)/page.tsx
+++ app/src/app/(main)/page.tsx
@@ -1,7 +1,12 @@
+import SearchBar from "../../components/searchbar";
+
 import Plane from "@/components/plane"
 
 
 export default function Home() {
+  return <div>
+    <SearchBar/>
+  </div>;
   return (
   <div>
     <Plane/>
--- app/src/components/searchbar.tsx
+++ app/src/components/searchbar.tsx
@@ -1,32 +1,40 @@
-import React, { useState } from 'react';
+"use client";
 
-const SearchBar: React.FC = () => {
-    const [userinput, setUserinput] = useState('');
+export default function SearchBar() {
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUserinput(event.target.value);
-    };
-
-    const handleSearch = (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log('Search query:', userinput);
-        // get search results based on userinput from api
-    };
-
-    return (
-        <form onSubmit={handleSearch} className="search-bar">
-            <input
-                type="text"
-                value={userinput}
-                onChange={handleInputChange}
-                placeholder="Search flights..."
-                className="search-input"
-            />
-            <button type="submit" className="search-button">
-                Search
-            </button>
-        </form>
-    );
-};
-
-export default SearchBar;
+  return (
+   <div className="grid grid-rows-2 justify-center">
+    <h1 className="font-bold text-xl">Find Cheap Flights</h1>
+    <div className="border-2 border-gray-300 rounded flex">
+        <input
+            type="text"
+            placeholder="From"
+            className="p-2 ml-0 m-2"
+        />
+        <input
+            type="text"
+            placeholder="To"
+            className="p-2 m-2"
+        />
+        <input
+            type="text"
+            placeholder="Departure"
+            className="p-2 m-2 w-24"
+        />
+        <input
+            type="text"
+            placeholder="Return"
+            className="p-2 m-2 w-20"
+        />
+        <input
+            type="text"
+            placeholder="Travelers"
+            className="p-2 m-2 w-24"
+        />
+        <button className="bg-blue-500 text-white py-2 px-4 rounded m-2">
+            Search
+        </button>
+    </div>
+   </div>
+  );
+}
