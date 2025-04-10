0f88acbd10d12b03a30a9a73973336c9d7c6d4d5
Amir Cox <amircoxx31@gmail.com>
2025-04-10 10:48:31 -0400

    made the signup and login pages center

--- app/src/app/(login)/login/page.tsx
+++ app/src/app/(login)/login/page.tsx
@@ -1,16 +1,18 @@
 export default function Login() {
-    return <div>
-        <h1 className="text-2xl text-blue-500 p-2 mt-1"><a href="/">NextFlight</a></h1>
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="flex flex-col justify-center items-center border-solid border-2 p-5 rounded-lg">
-                <h1 className="text-2xl text-blue-500">Login</h1>
-                <form className="flex flex-col gap-4 mt-5">
-                    <input type="text" placeholder="Username" className="border p-2 rounded" />
-                    <input type="password" placeholder="Password" className="border p-2 rounded" />
-                    <button type="submit" className="bg-blue-300 py-2 px-4 rounded-full hover:cursor-pointer">Login</button>
-                </form>
-                <p className="mt-4 text-sm">Don't have an account? <a href="/signup" className="text-blue-500">Signup</a></p>
-            </div>
+    return (
+      <div className="min-h-screen flex flex-col">
+        <h1 className="text-2xl text-blue-500 absolute top-0 left-0 p-2 mt-1"><a href="/">NextFlight</a></h1>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center border border-gray-200 p-5 rounded-lg shadow-md">
+            <h1 className="text-2xl text-blue-500">Login</h1>
+            <form className="flex flex-col gap-4 mt-5">
+              <input type="text" placeholder="Username" className="border p-2 rounded" />
+              <input type="password" placeholder="Password" className="border p-2 rounded" />
+              <button type="submit" className="bg-blue-300 py-2 px-4 rounded-full hover:cursor-pointer">Login</button>
+            </form>
+            <p className="mt-4 text-sm">Don't have an account? <a href="/signup" className="text-blue-500">Signup</a></p>
+          </div>
         </div>
-    </div>;
-}
+      </div>
+    );
+  }
