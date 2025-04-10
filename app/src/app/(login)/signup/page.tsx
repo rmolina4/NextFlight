da06960a716dea54e78a5754bbcca2400d069718
Amir Cox <amircoxx31@gmail.com>
2025-04-10 10:10:56 -0400

    added signup and login page

--- app/src/app/(login)/signup/page.tsx
+++ app/src/app/(login)/signup/page.tsx
@@ -1,3 +1,17 @@
 export default function Signup() {
-    return <div></div>;
+    return <div>
+        <h1 className="text-2xl text-blue-500 p-2 mt-1"><a href="/">NextFlight</a></h1>
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="flex flex-col justify-center items-center border-solid border-2 p-5 rounded-lg">
+                <h1 className="text-2xl text-blue-500">Signup</h1>
+                <form className="flex flex-col gap-4 mt-5">
+                    <input type="text" placeholder="Username" className="border p-2 rounded" />
+                    <input type="password" placeholder="Password" className="border p-2 rounded" />
+                    <button type="submit" className="bg-blue-300 py-2 px-4 rounded-full hover:cursor-pointer">Login</button>
+                </form>
+                <p className="mt-4 text-sm">Already have an account? <a href="/login" className="text-blue-500">Login</a></p>
+
+            </div>
+        </div>
+    </div>;
 }
