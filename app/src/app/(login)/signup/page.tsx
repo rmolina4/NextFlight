fd10dfd1fe9b34d9787929a6b7efc6d5a25436cf
Amir Cox <amircoxx31@gmail.com>
2025-04-10 13:21:57 -0400

    added redirect to homepage after signup or login button clicked

--- app/src/app/(login)/signup/page.tsx
+++ app/src/app/(login)/signup/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Signup() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const router = useRouter();
+  
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push("/");
+    }
+  }, [isLoggedIn, router]);
+  
     return (
       <div className="min-h-screen flex flex-col">
         <h1 className="text-2xl text-blue-500 absolute top-0 left-0 p-2 mt-1"><a href="/">NextFlight</a></h1>
         <div className="flex-1 flex justify-center items-center">
           <div className="flex flex-col justify-center items-center border border-gray-200 p-5 rounded-lg shadow-md">
             <h1 className="text-2xl text-blue-500">Signup</h1>
-            <form className="flex flex-col gap-4 mt-5">
+            <form className="flex flex-col gap-4 mt-5" onSubmit={handleSubmit}>
               <input type="text" placeholder="Username" className="border p-2 rounded" />
               <input type="email" placeholder="Email" className="border p-2 rounded" />
               <input type="password" placeholder="Password" className="border p-2 rounded" />
