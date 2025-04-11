fcbce503a1d61a17afc1b520e3f903fe61e00ad9
rmolina4 <rrmolina4@gmail.com>
2025-04-11 18:20:51 -0400

    initial is empty

--- app/src/app/(main)/page.tsx
+++ app/src/app/(main)/page.tsx
@@ -15,31 +15,8 @@ export interface Flight {
   key: number,
 }
 
-const initialArray = [    {
-  from: "USA",
-  to: "Spain",
-  departure: "5/5/2025",
-  return: "6/5/2025",
-  travelers: "1",
-  key: 0,
-}, {
-  from: "Canada",
-  to: "France",
-  departure: "7/7/2025",
-  return: "8/7/2025",
-  travelers: "2",
-  key: 1,
-}, {
-  from: "UK",
-  to: "Germany",
-  departure: "9/9/2025",
-  return: "10/9/2025",
-  travelers: "3",
-  key: 2,
-}];
-
 export default function Home() {
-  const [flights, setFlights] = useState<Flight[]>(initialArray);
+  const [flights, setFlights] = useState<Flight[]>([]);
 
   const handleSubmit = (flight: Flight) => {
     setFlights([...flights, flight]);
