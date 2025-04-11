359d7a0e746054524dcec1ae28db2f172e612fa4
Steven Tran <stevenhuyt@gmail.com>
2025-04-11 08:43:00 -0400

    Removed global CSS and used inline tailwind styling for study abroad components

--- app/src/components/abroadcards.tsx
+++ app/src/components/abroadcards.tsx
@@ -1,9 +1,10 @@
 export default function Abroadcards() {
     return (
-        <div className="IndivCard">
-            <h1>Program #</h1>
-            <p> Location</p>
-            <a href="Google.com"> Image</a>
-        </div>
-    )
-}
+        // Container for separate cards, with title, location, and image
+      <div className="flex m-5 bg-[#333] p-5 rounded-[10px] text-white items-center justify-between max-h-[65px] max-w-[300px] font-sans">
+        <h1 className="text-lg font-bold">Program #</h1>
+        <p className="text-sm text-gray-300">Location</p>
+        <a href="https://google.com" className="underline text-blue-400">Image</a>
+      </div>
+    );
+  }
