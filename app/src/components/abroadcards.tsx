0ac8e4d23ad5aa4509150d38e318fa8fa2c95dd1
rmolina4 <rrmolina4@gmail.com>
2025-04-10 22:43:09 -0400

    text on plane image

--- app/src/components/abroadcards.tsx
+++ app/src/components/abroadcards.tsx
@@ -1,4 +1,3 @@
-
 export default function Abroadcards() {
     return (
         <div className="IndivCard">
--- app/src/components/plane.tsx
+++ app/src/components/plane.tsx
@@ -3,15 +3,22 @@ import plane from "../assets/nick-morales-BwYcH78rcpI-unsplash.jpg";
 
 export default function Plane() {
   return (
-  <div className="w-full h-[50vh] overflow-hidden relative">
-    <Image
-      src={plane}
-      alt="Splash image"
-      quality={100}
-      priority
-      className="object-[0px_-1300px]"
-    />
-  <div className="absolute inset-0 bg-[linear-gradient(to_top,white_5%,transparent_40%)]" />
-</div>
-  )
+    <div className="w-full h-[50vh] overflow-hidden relative">
+      <Image
+        src={plane}
+        alt="Splash image"
+        quality={100}
+        priority
+        className="object-[0px_-1300px]"
+      />
+      <div className="absolute inset-0 bg-[linear-gradient(to_top,white_5%,transparent_40%)]" />
+      <h1 className="absolute top-40 left-25 text-white text-6xl font-bold drop-shadow-lg">
+        Fly Cheap
+      </h1>
+      <span className="absolute top-55 left-25 text-white text-xl font-bold drop-shadow-lg max-w-xl">
+        NextFlight is your shortcut to finding affordable flights. Discover more.  
+        Spend Less. Fly cheap with NextFlight.
+      </span>
+    </div>
+  );
 }
