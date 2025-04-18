71c8ed560eb33678ba66756de02e641722795564
rmolina4 <rrmolina4@gmail.com>
2025-04-18 19:22:21 -0400

    bug fix

--- app/src/app/(main)/search/page.tsx
+++ app/src/app/(main)/search/page.tsx
@@ -31,9 +31,32 @@ export default function Search() {
   const [flights, setFlights] = useState<Flight[]>([]);
   const { data: session } = useSession();
   const isLoggedIn = !!session?.user;
-  
-  const handleSubmit = (flight: Flight) => {
-    setFlights([...flights, flight]);
+
+  const handleSubmit = async (search: Search) => {
+    const token = process.env.NEXT_PUBLIC_TOKEN;
+    const res = await fetch(
+      `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=${search.origin}&maxPrice=200`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      console.error("API request failed");
+    }
+
+    const { data } = await res.json();
+
+    setFlights(
+      data.map((item: any, index: number) => ({
+        ...item,
+        key: index,
+        seat: "9A",
+      }))
+    );
   };
 
   return (
