ca7561d42978e46cb17c723a98a83f4204c065a6
rmolina4 <rrmolina4@gmail.com>
2025-04-20 12:27:38 -0400

    user schema

--- app/src/models/userSchema.ts
+++ app/src/models/userSchema.ts
@@ -22,7 +22,11 @@ const userSchema: Schema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  flights: [flightSchema],
+  flights: {
+    type: [flightSchema],
+    required: false,
+    default: [],
+  },
 });
 
 const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
