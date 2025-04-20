29da9afecafb1f6ca8edad6fea269ecc4a6d3424
Amir Cox <amircoxx31@gmail.com>
2025-04-20 11:00:43 -0400

    fixed auth

--- app/src/app/actions/index.ts
+++ app/src/app/actions/index.ts
@@ -1,25 +1,24 @@
-"use server";
+'use server'
 
 import { signIn, signOut } from "../auth";
 
-interface loginInfo {
-  username: string;
-  password: string;
+export async function doLogout () {
+    await signOut({ redirectTo: "/" });
 }
 
-export async function logout() {
-  await signOut({ redirectTo: "/" });
-}
+export async function doCredentialLogin (formData: FormData) : Promise<any> {
 
-export async function login(data: loginInfo): Promise<any> {
-  try {
-    const response = await signIn("credentials", {
-      username: data.username,
-      password: data.password,
-      redirect: false,
-    });
-    return response;
-  } catch (err: any) {
-    return {error: { message: err.message || "Login failed." } };
-  }
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+
+    try {
+        const response = await signIn("credentials", {
+            username,
+            password,
+            redirect: false,
+        });
+        return response;
+    } catch (err : any) {
+        return {error: { message: err.message || "Login failed." } };
+    }
 }
