0cc9bc053913133629a14320e03712a9882960f1
rmolina4 <rrmolina4@gmail.com>
2025-04-22 02:05:11 -0400

    image responsiveness

--- app/src/app/(main)/page.tsx
+++ app/src/app/(main)/page.tsx
@@ -15,23 +15,23 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Plane />
-      <div className="flex justify-center items-center flex-1 mb-20 gap-4">
+      <div className="flex justify-center items-center flex-1 mb-20 gap-4 text-xs sm:text-xl">
         <div className="flex-1 flex flex-col justify-center items-center text-center border border-black p-5 rounded-lg border-2 mx-4">
           <div className="flex justify-center items-center">
             <Icon icon="tabler:user" className="text-3xl pb-1" />
           </div>
-          <h3 className="text-xl">
+          <h3>
             Over 45,000 flights and roughly 2.9 million airline passengers fly
             daily.
           </h3>
         </div>
         <div className="flex flex-col flex-2 max-w-2xl text-center p-5 mx-4">
-          <h1 className="font-bold text-4xl pb-2">Fly Cheap</h1>
-          <h3 className="text-xl">
+          <h1 className="font-bold text-2xl sm:text-4xl pb-2">Fly Cheap</h1>
+          <h3>
             NextFlight is your shortcut to finding affordable flights. We scan
             thousands of airlines to find you the best deals possible.
           </h3>
-          <h3 className="text-xl">
+          <h3>
             Discover more. Spend Less. Fly cheap with NextFlight.
           </h3>
         </div>
@@ -40,7 +40,7 @@ export default function Home() {
           <div className="flex justify-center items-center">
             <Icon icon="tabler:user" className="text-3xl pb-1" />
           </div>
-          <h3 className="text-xl">
+          <h3>
             We have helped at least one person in finding the most affordable
             flight.
           </h3>
--- app/src/components/largePlane.tsx
+++ app/src/components/largePlane.tsx
@@ -9,9 +9,10 @@ export default function Plane() {
         alt="Splash image"
         quality={100}
         priority
-        className="xl:object-[0px_-230px] 2xl:object-[0px_-250px]"
+        fill
+        className="object-cover xl:object-[0px_-230px] 2xl:object-[0px_-250px]"
       />
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold drop-shadow-lg text-white">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold drop-shadow-lg text-white text-center">
         About Us
       </h1>
     </div>
