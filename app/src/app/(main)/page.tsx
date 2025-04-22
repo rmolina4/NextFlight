07399852c57a430c4ba52b3af734a6f2baed0022
rmolina4 <rrmolina4@gmail.com>
2025-04-22 18:56:29 -0400

    about page change

--- app/src/app/(main)/page.tsx
+++ app/src/app/(main)/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Plane />
-      <div className="flex justify-center items-center flex-1 mb-20 gap-4 text-xs sm:text-xl">
-        <div className="flex-1 flex flex-col justify-center items-center text-center border border-black p-5 rounded-lg border-2 mx-4">
+      <div className="flex justify-center items-center flex-1 mb-20 gap-4 text-xs sm:text-lg">
+        <div className="flex flex-col flex-1 justify-center items-center text-center border border-black border-1 p-5 rounded-3xl mx-4 min-h-40 max-h-50">
           <div className="flex justify-center items-center">
-            <Icon icon="tabler:user" className="text-3xl pb-1" />
+            <Icon icon="tabler:user" className="text-2xl sm:text-3xl pb-1" />
           </div>
           <h3>
             Over 45,000 flights and roughly 2.9 million airline passengers fly
@@ -31,18 +31,15 @@ export default function Home() {
             NextFlight is your shortcut to finding affordable flights. We scan
             thousands of airlines to find you the best deals possible.
           </h3>
-          <h3>
-            Discover more. Spend Less. Fly cheap with NextFlight.
-          </h3>
+          <h3>Discover more. Spend Less. Fly cheap with NextFlight.</h3>
         </div>
-
-        <div className="flex flex-col flex-1 max-w-2xl text-center border border-black border-black p-5 rounded-lg border-2 mx-4">
+        <div className="flex flex-col flex-1 justify-center items-center text-center border border-black border-1 p-5 rounded-3xl mx-4 min-h-40 max-h-50">
           <div className="flex justify-center items-center">
-            <Icon icon="tabler:user" className="text-3xl pb-1" />
+            <Icon icon="tabler:user" className="text-2xl sm:text-3xl pb-1" />
           </div>
           <h3>
-            We have helped at least one person in finding the most affordable
-            flight.
+            We're proud to have helped countless travelers find the most
+            affordable flights.
           </h3>
         </div>
       </div>
